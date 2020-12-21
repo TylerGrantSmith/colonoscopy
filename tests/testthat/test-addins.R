e446f50c9ed22459dbdc0989bd76074d3bfaf9dc
Tyler Smith <39497067+TylerGrantSmith@users.noreply.github.com>
2020-12-20 20:24:16 -0600

    fix mock check error

--- tests/testthat/test-addins.R
+++ tests/testthat/test-addins.R
@@ -1,6 +1,6 @@
 testthat::test_that("scope_selection", {
   testthat::with_mock(
-    check_rstudio = function() FALSE,
+    "colonoscopy::check_rstudio" = function() FALSE,
     testthat::expect_error(colonoscopy::scope_selection())
   )
 })
